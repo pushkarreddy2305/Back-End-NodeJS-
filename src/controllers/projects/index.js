96dce5341f1152c4007c12d5919b71fa826b89cd
Pushkar Karakala <pkarakala@dxc.com>
2019-07-01 16:03:09 -0500

    added getbyid api and resolved cors issue for put method

--- src/controllers/projects/index.js
+++ src/controllers/projects/index.js
@@ -1,48 +1,51 @@
-'use strict';
-const {projectModel} = require("../../models");
+"use strict";
+const { projectModel } = require("../../models");
 
-function create(name,description){
-    var newProj = new projectModel({
-        id,
-        name,
-        description,
+function create(name, description) {
+  var newProj = new projectModel({
+    name,
+    description
+  });
+  newProj.save();
+  return newProj;
+}
+
+function findAll() {
+  return projectModel.find().exec();
+}
+
+function find(search) {
+  var regex = new RegExp(search, "i");
+  return projectModel
+    .find({
+      $or: [{ description: regex }, { name: regex }]
     })
-    newProj.save();
-    return newProj;
+    .exec();
 }
 
-function findAll(){
-    return projectModel.find().exec();
+function findById(id) {
+  return projectModel.findOne({ _id: id }).exec();
 }
 
-function find(search){
-    var regex = new RegExp(search,'i');
-    return projectModel.find({
-        $or:[
-            {description:regex},
-            {name:regex}
-        ]
-    }).exec();
-}
-
-async function edit(id,name,description){
-    return await projectModel.findOne({_id:id}).exec()
-        .then(proj => {
-            proj.name = name;
-            proj.description = description;
-            return proj.save();
-        })
-        .catch(
-            err=> {
-                console.log("edit error:", err)
-                reject(err);
-            }
-        );
+async function edit(id, name, description) {
+  return await projectModel
+    .findOne({ _id: id })
+    .exec()
+    .then(proj => {
+      proj.name = name;
+      proj.description = description;
+      return proj.save();
+    })
+    .catch(err => {
+      console.log("edit error:", err);
+      reject(err);
+    });
 }
 
 module.exports = {
-    create,
-    find,
-    findAll,
-    edit,
-}
+  create,
+  find,
+  findAll,
+  edit,
+  findById
+};
