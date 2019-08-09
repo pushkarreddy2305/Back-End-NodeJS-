0b7b0c1a452522c34526956d80663e856554ecb7
elbadryd <elbadry.d@gmail.com>
2019-08-09 16:43:09 -0500

    delete template by id

--- src/routes/templates/index.js
+++ src/routes/templates/index.js
@@ -13,17 +13,14 @@ router.get('/', (req, res) =>{
     })
 })
 
+
 router.post('/', async (req, res) =>{
     const { name, location, type } = req.body;
     let id; 
-    await TemplateType.findOne({typeName: 'file'})
+    await TemplateType.findOne({typeName: type})
         .exec()
         .then(type=> id = type._id)
-        .catch(err => res.send(err))
-    // TemplateType.findOne({typeName:type}, (err, type) =>{
-    //     if (err){
-    //        res.send('cannot find file type')
-    //     } else {
+        .catch(err => res.send('cannot find file type'))
             return new Template({
                 templateName: name,
                 templateLocation: location,
@@ -33,22 +30,41 @@ router.post('/', async (req, res) =>{
                 res.send(result)
             })
             .catch(err=> res.send(err))
-        // }
-    // })
 })
 
-router.put('/:id', (req, res)=>{
-    const id = req.params.id
+router.put('/:id', async (req, res)=>{
+    const _id = req.params.id
     const { name, location, type } = req.body
+    let typeId; 
+    await TemplateType.findOne({typeName: type})
+        .exec()
+        .then(type=> typeId = type._id)
+        .catch(err => res.send('cannot find file type'))
     let original;
-    Template.findbyId(id)
+    await Template.findById(_id)
         .exec()
         .then(template=> original = template)
-        .catch(res.send('cannot find template'))
+        .catch(err => res.send('cannot find template'))
     let newData = {
-        name: name || original.name,
-        location: location || original.location,
-        type: type || original.type
-    }
+        templateName: name || original.templateName,
+        templateLocation: location || original.templateLocation,
+        templateType: typeId || original.templateType
+    };
+    console.log(newData, original)
+   return Template.updateOne({_id}, newData, (err, doc)=>{
+       if (err){
+           res.send(err)
+       } else {
+           res.send(doc)
+       }
+   })
 })
+
+router.delete('/:id', (req, res)=> {
+    const _id = req.params.id;
+    return Template.deleteOne({_id}).exec()
+    .then(temp=> res.send(temp))
+    .catch(err=>res.send(err))
+})
+
 module.exports = router;
